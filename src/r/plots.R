8a7ed417200b123b96532be76f0c59122384602b
Indiana Nikel <indiana.nikel@gmail.com>
2017-12-08 00:45:46 -0800

    Initial set up on plots.R file, libraries, error handling, and structure

--- src/r/plots.R
+++ src/r/plots.R
@@ -1,6 +1,21 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
+# Error handling for arguments
 if (length(args) != 2) {
   stop("Two arguments must be supplied (input_path, output_path).", call.=FALSE)
 }
+
+# Initialize R enviroment, including libraries
+setwd("../..")
+library(tidyverse)
+library(cowplot)
+
+# Read in summarized data as csv
+data <- read.csv(arg[1], sep=",")
+
+# Create plots
+
+
+
+# Write plots as png
