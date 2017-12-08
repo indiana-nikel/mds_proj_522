12e06f7f86d622f73b3bea84ccdb46544a51ba20
Indiana Nikel <indiana.nikel@gmail.com>
2017-12-08 00:39:54 -0800

    Complete analysis script with comments, fully functioning

--- src/r/analysis.R
+++ src/r/analysis.R
@@ -1,22 +1,25 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
+# Error handling for arguments
 if (length(args) != 2) {
   stop("Two arguments must be supplied (input_path, output_path).", call.=FALSE)
 }
 
+# Initial set up of R environment, including libraries
 library(tidyverse)
 setwd("../..")
 
-print(arg[1])
-print(arg[2])
+# Read in data
+data <- read.csv(args[1], sep=",")
 
-data <- read.table(arg[1])
-
+# Summarize data, removing unused columns
 data_summarized <- 
   data %>% 
   select(-description, -taster_name, -taster_twitter_handle)
 
+# Handle NA values by removing the rows
 data_no_na <- na.omit(data_summarized)
 
-write.csv(data_no_na, arg[2])
+# Writes data to new csv
+write.csv(data_no_na, args[2])
