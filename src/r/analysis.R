ffb2ce23118a5748fd9801d8fa2256ca5a180693
Indiana Nikel <indiana.nikel@gmail.com>
2017-12-08 00:34:13 -0800

    first attempt at summarizing data

--- src/r/analysis.R
+++ src/r/analysis.R
@@ -4,3 +4,19 @@ args = commandArgs(trailingOnly=TRUE)
 if (length(args) != 2) {
   stop("Two arguments must be supplied (input_path, output_path).", call.=FALSE)
 }
+
+library(tidyverse)
+setwd("../..")
+
+print(arg[1])
+print(arg[2])
+
+data <- read.table(arg[1])
+
+data_summarized <- 
+  data %>% 
+  select(-description, -taster_name, -taster_twitter_handle)
+
+data_no_na <- na.omit(data_summarized)
+
+write.csv(data_no_na, arg[2])
